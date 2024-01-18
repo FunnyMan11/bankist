a0f9933452c2d76d2957842ce53293adbe333b28
macbook air <macbookair@macbooks-MacBook-Air-2.local>
2024-01-18 22:11:13 +0400

    more advanced dom manipulation examples

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-'use strict';
-
 ///////////////////////////////////////
 // Modal window
 
@@ -36,7 +34,7 @@ console.log(document.documentElement);
 console.log(document.head);
 console.log(document.body);
 
-document.querySelector('.header');
+const header = document.querySelector('.header');
 const allSections = document.querySelectorAll('.section');
 console.log(allSections);
 
@@ -44,3 +42,58 @@ document.getElementById('section--1')
 const allButtons = document.getElementsByTagName('button');
 
 console.log(allButtons)
+
+console.log(document.getElementsByClassName("btn"));
+
+const message = document.createElement('div')
+message.classList.add('cookie-message')
+
+message.innerHTML = 'we use cookies for improved functionality and analytics <button class="btn btn--close-cookie"> Got it! </button>';
+
+// if you use either both of them message dissapears only once on the bottom on the page
+// header.prepend(message); // first child
+header.append(message) // last child 
+// header.prepend(message.cloneNode(true)) solution ! 
+
+// header.before(message); // message is before header element 
+// header.after(message); // header is after header element
+
+//deleting element
+document
+    .querySelector('.btn--close-cookie')
+    .addEventListener('click', function(){
+    message.remove();
+    // message.parentElement.removeChild(message) // second way
+})
+
+//styles
+
+message.style.backgroundColor = "#37383d"
+message.style.width = "120%";
+
+console.log(message.style.backgroundColor); // logged 
+console.log(message.style.color) // not logged inline style
+
+console.log(getComputedStyle(message)) //all styles 
+console.log(getComputedStyle(message).color) // color property
+
+document.documentElement.style.setProperty('--color-primary', 'orangered') // all element which have property --color-primary, changed to oranged
+
+// attributes 
+
+const logo = document.querySelector('.nav__logo')
+console.log(logo.alt)
+console.log(logo.src) // absolute path
+console.log(logo.className)
+
+console.log(logo.getAttribute('src')); // relative path
+
+// same as links 
+const link = document.querySelector('.twitter-link')
+console.log(link.href); // same 
+console.log(link.getAttribute('href')); // same because of they are absolutes 
+
+// but not for buttons, use this 
+// console.log(link.getAttribute('href'));
+
+
